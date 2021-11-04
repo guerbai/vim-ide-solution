9d25e9b72a9ae06ec7111b0d8e3d977d596b33af
guerbai <guerbai8@gmail.com>
2021-11-04 22:46:46 +0800

    refactor key binding, use more nvim plugin

--- lua/plugins/configs/coc.lua
+++ lua/plugins/configs/coc.lua
@@ -2,18 +2,26 @@ function silent_map(mode, f, t)
   vim.api.nvim_set_keymap(mode, f, t, { silent = true })
 end
 
-silent_map('n', '<Leader>gd', '<Plug>(coc-definition)', {silent=true})
-silent_map('n', '<Leader>gr', '<Plug>(coc-references)', {silent=true})
-silent_map('n', '<Leader>gK', ":call CocActionAsync('doHover')<CR>", {silent=true})
-silent_map('n', '<Leader>g[', '<Plug>(coc-diagnostic-prev)', {silent=true})
-silent_map('n', '<Leader>g]', '<Plug>(coc-diagnostic-next)', {silent=true})
-silent_map('n', '<Leader>er', ':CocList diagnostics<CR>', {silent=true})
-silent_map('n', '<Leader>qf', '<Plug>(coc-fix-current)', {silent=true})
+silent_map('n', '<Leader>gd', '<Plug>(coc-definition)')
+silent_map('n', '<Leader>gr', '<Plug>(coc-references)')
+silent_map('n', '<Leader>gK', ":call CocActionAsync('doHover')<CR>")
+silent_map('n', '<Leader>g[', '<Plug>(coc-diagnostic-prev)')
+silent_map('n', '<Leader>g]', '<Plug>(coc-diagnostic-next)')
+silent_map('n', '<Leader>ge', ':CocList diagnostics<CR>')
+silent_map('n', '<Leader>gf', '<Plug>(coc-fix-current)')
 -- silent_map('n', '<Leader>lt', '<Plug>(coc-format)', {silent=true})
-silent_map('n', '<Leader>rn', '<Plug>(coc-rename)', {silent=true})
+silent_map('n', '<Leader>rn', '<Plug>(coc-rename)')
+silent_map('i', '<C-l>', '<Plug>(coc-snippets-expand)')
 -- vim.cmd 'inoremap <expr> <Tab> pumvisible() ? "\\<C-n>" : "\\<Tab>"'
 -- vim.cmd 'inoremap <expr> <S-Tab> pumvisible() ? "\\<C-p>" : "\\<S-Tab>"'
+-- vim.cmd "autocmd BufWritePre * :silent call CocAction('format')"
 
 vim.cmd "autocmd BufWritePre * :silent call CocAction('runCommand', 'editor.action.organizeImport')"
--- vim.cmd "autocmd BufWritePre * :silent call CocAction('format')"
 vim.cmd "autocmd BufNewFile,BufRead *.go setlocal noexpandtab tabstop=4 shiftwidth=4"
+
+vim.api.nvim_command([[
+inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
+]])
+vim.api.nvim_command([[
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+]])
--- lua/plugins/configs/fzf.lua
+++ lua/plugins/configs/fzf.lua
@@ -1,10 +1,16 @@
-vim.api.nvim_set_keymap('n', '<Leader>f', ':FZF<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':FZF<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>fw', ':Rg<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>fm', ':Maps<CR>', {noremap = true})
 
 vim.api.nvim_command([[
-command! -bang -nargs=* Rg call fzf#vim#grep( "rg --column --line-number --no-heading --color=always --smart-case -- ".shellescape(<q-args>), 1,   fzf#vim#with_preview('up', 'ctrl-/'), 1)
+command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case -g '!{thrift_gen,kitex_gen,clients}' -- ".shellescape(<q-args>), 1, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}, 'up:60%', 'ctrl-/'), <bang>0)
 ]])
-vim.api.nvim_command([[
-command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case -- ".shellescape(<q-args>), 1, fzf#vim#with_preview('up', 'ctrl-/'), <bang>0)
-]])
+-- vim.api.nvim_command([[
+-- command! -bang -nargs=* Rg call fzf#vim#grep( "rg --column --line-number --no-heading --color=always --smart-case -- ".shellescape(<q-args>), 1,   fzf#vim#with_preview('up', 'ctrl-/'), 1)
+-- ]])
+-- vim.api.nvim_command([[
+-- command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case -- ".shellescape(<q-args>), 1, fzf#vim#with_preview('up', 'ctrl-/'), <bang>0)
+-- ]])
+-- vim.api.nvim_command([[
+-- command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, '--color-path="0;33"', <bang>0)
+-- ]])
